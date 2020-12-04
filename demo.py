2ee245a231f3bd65f6a834881c2cccc65820360a
Trinkle23897 <trinkle23897@gmail.com>
2020-12-04 11:41:12 +0800

    timeline

--- demo.py
+++ demo.py
@@ -1,13 +1,14 @@
-import altair as alt
 from collections import Counter
+from datetime import datetime
+from google.cloud import bigquery
+from multiprocessing import Pool
+import altair as alt
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-
-from google.cloud import bigquery
+import time
 
 # global variables definition
 
@@ -16,6 +17,11 @@ MAX_WIDTH = 700
 # update the secret to env
 # this credential file should not commit to github
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gcloud.json'
+
+# user info metadata
+table_name = 'badges comments posts_answers posts_questions users'.split()
+user_id_str = 'user_id user_id owner_user_id owner_user_id id'.split()
+date_str = 'date creation_date creation_date creation_date /'.split()
 
 # Create a "Client" object
 client = bigquery.Client()
@@ -40,226 +46,230 @@ def serialization(id):
 
 
 @st.cache
-def get_qid2uid():
-    return np.load('qid2uid.npy')
+def get_table(table_name, max_results=5):
+    table_ref = dataset_ref.table(table_name)
+    table = client.get_table(table_ref)
+    return client.list_rows(table, max_results=max_results).to_dataframe()
 
 
-qid2uid = get_qid2uid()
+@st.cache(hash_funcs={bigquery.Client: lambda _: None})
+def get_query(query):
+    print(time.asctime(), query)
+    return bigquery.Client().query(query, job_config=safe_config).to_dataframe()
 
 
-@st.cache
-def get_table(table_name, max_results=10000):
-    # Construct a reference to the "posts_answers" table
-    answers_table_ref = dataset_ref.table(table_name)
-
-    # API request - fetch the table
-    answers_table = client.get_table(answers_table_ref)
-
-    # Preview the first five lines of the "posts_answers" table
-    return client.list_rows(answers_table, max_results=max_results).to_dataframe()
-
-
-@st.cache
-def get_all_users(location=None, user_id=None, reputation_thres=None):
-    if location:
-        questions_query = f"WHERE location IN {serialization(location)}"
-    elif user_id:
-        questions_query = f"WHERE id IN {serialization(user_id)}"
-    elif reputation_thres:
-        questions_query = f"WHERE reputation >= {reputation_thres}"
-    else:
-        questions_query = ""
-    questions_query = f"SELECT id, display_name, reputation, up_votes, down_votes FROM `bigquery-public-data.stackoverflow.users` " + questions_query
-
-    # Set up the query (cancel the query if it would use too much of
-    # your quota, with the limit set to 1 GB)
-    questions_query_job = client.query(questions_query, job_config=safe_config)
-
-    # API request - run the query, and return a pandas DataFrame
-    questions_results = questions_query_job.to_dataframe()
-
-    return questions_results
+@st.cache(hash_funcs={bigquery.Client: lambda _: None})
+def qid2title(qid):
+    qid = sorted(list(set(qid)))
+    query1 = f"""
+        SELECT id, parent_id from `bigquery-public-data.stackoverflow.posts_answers`
+        WHERE id in {serialization(qid)}"""
+    query2 = f"""
+        SELECT id, title from `bigquery-public-data.stackoverflow.posts_questions`
+        WHERE id in {serialization(qid)}"""
+    df1, df2 = Pool(2).map(get_query, [query1, query2])
+    aid2qid = {a: p for a, p in zip(df1['id'], df1['parent_id'])}
+    qid2title = {i: t for i, t in zip(df2['id'], df2['title'])}
+    return {q: qid2title.get(aid2qid.get(q, q), "this question") for q in qid}
 
 
-@st.cache
-def get_all_tags():
-    questions_query = f"SELECT id, tag_name FROM `bigquery-public-data.stackoverflow.tags`"
-
-    # Set up the query (cancel the query if it would use too much of
-    # your quota, with the limit set to 1 GB)
-    questions_query_job = client.query(questions_query, job_config=safe_config)
-
-    # API request - run the query, and return a pandas DataFrame
-    questions_results = questions_query_job.to_dataframe()
-
-    # return a list of tags
-    return list(questions_results.iloc[:, 1])
+def show_raw():
+    for table in list(client.list_tables(dataset)):
+        data = get_table(table.table_id)
+        st.write(table.table_id, data)
 
 
-@st.cache
-def get_questions_by_user(user_id):
-    questions_query = f"SELECT * FROM `bigquery-public-data.stackoverflow.posts_questions` \
-                        WHERE owner_user_id IN {serialization(user_id)}"
-    # Set up the query (cancel the query if it would use too much of
-    # your quota, with the limit set to 1 GB)
-    questions_query_job = client.query(questions_query, job_config=safe_config)
-
-    # API request - run the query, and return a pandas DataFrame
-    questions_results = questions_query_job.to_dataframe()
-
-    return questions_results
+@st.cache(hash_funcs={bigquery.Client: lambda _: None})
+def get_user_timeline(user_id, start=datetime(2019, 9, 10), end=datetime(2020, 9, 10)):
+    if not isinstance(user_id, list):
+        user_id = [user_id]
+    if len(user_id) == 0:
+        return {}
+    start, end = start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')
+    query_list = []
+    for uid in user_id:
+        for t, u, d in zip(table_name, user_id_str, date_str):
+            query = f'SELECT * FROM `bigquery-public-data.stackoverflow.{t}` WHERE {u} = {uid}'
+            if d != '/':
+                query += f" AND {d} BETWEEN '{start}' AND '{end}'"
+            query_list.append(query)
+    # single process
+    # result = {}
+    # for t, q in zip(table_name, query_list):
+    #     result[t] = get_query(q)
+    # return result
+    # multiple process
+    raw = Pool(len(query_list)).map(get_query, query_list)
+    result = {}
+    for uid in user_id:
+        result[uid] = {}
+        for t, r in zip(table_name, raw[:len(table_name)]):
+            result[uid][t] = r
+        raw = raw[len(table_name):]
+    return result
 
 
 @st.cache
-def get_answered_questions_by_user(user_id):
-    questions_query = f"SELECT a.id, a.body, a.owner_user_id AS uid, q.id AS qid \
-                FROM `bigquery-public-data.stackoverflow.posts_questions` AS q \
-                INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a \
-                    ON q.id = a.parent_id \
-                WHERE a.owner_user_id IN {serialization(user_id)}"
-    # Set up the query (cancel the query if it would use too much of
-    # your quota, with the limit set to 1 GB)
-    questions_query_job = client.query(questions_query, job_config=safe_config)
-
-    # API request - run the query, and return a pandas DataFrame
-    questions_results = questions_query_job.to_dataframe()
-
-    # Preview results
-    return questions_results
-
-
-@st.cache
-def get_tag_by_question(qid):
-    questions_query = f"SELECT tags \
-                FROM `bigquery-public-data.stackoverflow.posts_questions` \
-                WHERE id IN {serialization(qid)}"
-    # Set up the query (cancel the query if it would use too much of
-    # your quota, with the limit set to 1 GB)
-    questions_query_job = client.query(questions_query, job_config=safe_config)
-
-    # API request - run the query, and return a pandas DataFrame
-    questions_results = questions_query_job.to_dataframe()
-
-    res = questions_results['tags']
-    return res
+def getweek(t: datetime) -> int:
+    w = t.weekofyear
+    if t.day_name() == 'Sunday':
+        w += 1
+    if t.month == 1 and w > 40:
+        p = t + pd.Timedelta(days=1)
+        wp = getweek(p)
+        if p.day_name() == 'Sunday':
+            w = wp - 1
+        else:
+            w = wp
+    elif t.month == 12 and w < 10:
+        p = t - pd.Timedelta(days=1)
+        wp = getweek(p)
+        if p.day_name() == 'Saturday':
+            w = wp + 1
+        else:
+            w = wp
+    return w
 
 
-@st.cache
-def get_question_tag_by_user(user_id):
-    tag_query = f"SELECT tags \
-        FROM `bigquery-public-data.stackoverflow.posts_questions` \
-        WHERE owner_user_id IN {serialization(user_id)}"
-    questions_query_job = client.query(tag_query, job_config=safe_config)
-    questions_results = questions_query_job.to_dataframe()
-    return questions_results
+def get_single_user_timeline(data: dict, ph=st) -> None:
+    """
+    :param data: the piece of output of get_user_timeline, {table: df}
+    :param ph: placeholder, whether to write the result
+    """
+    result = []
+    for d, v in zip(date_str, data.values()):
+        if d != '/':
+            result += v[d].to_list()
+    np.save('test', result)
+    name = data["users"]["display_name"][0]
+    for y in reversed(list(range(min(result).year, max(result).year + 1))):
+        raw = []
+        # fill valid data
+        for t in result:
+            if t.year == y:
+                raw.append((t, getweek(t), 1))
+        # fill null data
+        t, delta, end = pd.Timestamp(y, 1, 1), pd.Timedelta(
+            days=1), pd.Timestamp(y + 1, 1, 1)
+        while t < end:
+            raw.append((t, getweek(t), 0))
+            t += delta
+        df = pd.DataFrame(raw, columns=['time', 'week', 'Contribution'])
+        ph.write(alt.Chart(df).mark_circle().encode(
+            x=alt.X('week:O', axis=alt.Axis(title='Week')),
+            y=alt.Y('day(time):O', axis=alt.Axis(title='Day')),
+            size=alt.Size('sum(Contribution):Q', legend=None),
+            color=alt.Color('sum(Contribution):N',
+                            scale=alt.Scale(scheme="greens")),
+            tooltip=['yearmonthdate(time)', 'sum(Contribution)'],
+        ).properties(title=f'{name}\'s Contributions in Year {y}'))
 
 
-@st.cache
-def get_answer_tag_by_user(user_id):
-    tag_query = f"SELECT q.tags \
-    FROM `bigquery-public-data.stackoverflow.posts_questions` AS q \
-    INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a \
-        ON q.id = a.parent_id \
-    WHERE a.owner_user_id IN {serialization(user_id)}"
-    questions_query_job = client.query(tag_query, job_config=safe_config)
-    questions_results = questions_query_job.to_dataframe()
-    return questions_results
+def convert(title: str) -> str:
+    return title.replace('<', '').replace('>', '')
 
 
-@st.cache
-def process_tags(tag_list):
-    tags = []
-    for i in tag_list:
-        tags += i.split('|')
-    return Counter(tags)
+def get_multi_user_timeline(data: dict) -> None:
+    """
+    :param data: the full output of get_user_timeline, {uid: {table: df}}
+    """
+    uid2name = {u: data[u]['users']["display_name"][0] for u in data}
+    table_date = {t: d for t, d in zip(table_name, date_str)}
+    qid = []
+    qid2t = {}
+    raw = []
+    for uid, user_data in data.items():
+        for tname, table in user_data.items():
+            if tname == 'users':
+                continue
+            for _, i in table.iterrows():
+                raw_single = [i[table_date[tname]], tname, uid]
+                if tname == 'badges':
+                    raw_single += [[i['name']]]
+                elif tname == 'comments':
+                    raw_single += [[i['post_id']]]
+                    qid.append(i['post_id'])
+                elif tname == 'posts_answers':
+                    raw_single += [[i['parent_id']]]
+                    qid.append(i['parent_id'])
+                elif tname == 'posts_questions':
+                    raw_single += [[i['id']]]
+                    qid2t[i['id']] = i['title']
+                raw.append(raw_single)
+    qid2t.update(qid2title(qid))
+    raw = sorted(raw)[::-1]
+    stack = []
+    for info in raw:
+        if len(stack) == 0:
+            stack.append(info)
+            continue
+        tm1, tname1, uid1, info1 = info
+        tm0, tname0, uid0, info0 = stack[-1]
+        if tm1 - tm0 >= pd.Timedelta(days=2) or tname1 != tname0 or uid1 != uid0:
+            stack.append(info)
+            continue
+        # merge
+        stack[-1] = [tm1, tname0, uid0, info0 + info1]
+    # build html
+    template = """
+<div class="card border-light">
+  <div class="card-body">
+    <h5 class="card-title"><i class="fa ICON" aria-hidden="true"></i> USERNAME</h5>
+    <p class="card-text">TEXT</p>
+    <p class="card-text text-right"><small class="text-muted">TIME</small></p>
+  </div>
+</div>
+<div class="container py-1"></div>"""
+    content = ''
+    for info in stack:
+        tm, tname, uid, info = info
+        username = f'<a href="https://stackoverflow.com/users/{uid}/" target="_blank">{uid2name[uid]}</a>' 
+        if tname == 'badges':
+            icon = 'fa-check-square-o'
+            info = 'Your friend got ' + ', '.join([f'{v}x {k} badge{"s" if v > 1 else ""}' for k, v in Counter(info).items()]) + ', congratulations!'
+        elif tname == 'comments':
+            icon = 'fa-commenting-o'
+            info = 'Your friend posted ' + ', '.join([f'{v} comment{"s" if v > 1 else ""} in <a href="https://stackoverflow.com/questions/{k}" target="_blank">{convert(qid2t.get(k, "this question"))}</a>' for k, v in Counter(info).items()]) + '.'
+        elif tname == 'posts_answers':
+            icon = 'fa-align-left'
+            info = 'Your friend posted ' + ', '.join([f'{v} answer{"s" if v > 1 else ""} in <a href="https://stackoverflow.com/questions/{k}" target="_blank">{convert(qid2t.get(k, "this question"))}</a>' for k, v in Counter(info).items()]) + '.'
+        elif tname == 'posts_questions':
+            icon = 'fa-question-circle-o'
+            info = f'Your friend posted {len(info)} question{"s" if len(info) > 1 else ""}: ' + ', '.join([f'<a href="https://stackoverflow.com/questions/{k}" target="_blank">{convert(qid2t[k])}</a>' for k in info]) + '.'
+        content += template.replace('ICON', icon).replace('USERNAME', username).replace(
+            'TIME', tm.strftime("%Y/%m/%d")).replace('TEXT', info)
+    st.components.v1.html("""
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<style>
+.card{border-radius: 4px;background: #fff;box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);padding: 14px 80px 18px 36px;cursor: pointer;}
+.card:hover{transform: scale(1.05);box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);}
+</style>
+<div class="text-center py-5"><button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Launch App!</button></div>
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="mx-5 py-5">
+CONTENT
+        <div class="text-center py-1"><small class="text-muted">No more result</small></div>
+      </div>
+    </div>
+  </div>
+</div>
+    """.replace('CONTENT', content), height=900, scrolling=True)
 
 
-@st.cache
-def get_tag_freq_by_user(user_id):
-    qt = get_question_tag_by_user(user_id)
-    at = get_answer_tag_by_user(user_id)
-    freq = process_tags(qt['tags'].to_list() + at['tags'].to_list())
-    return freq
+if __name__ == '__main__':
+    if st.checkbox('Show raw data'):
+        show_raw()
+    a = st.slider(
+        'Select date range', datetime(2008, 8, 1), datetime(2020, 9, 10),
+        value=(datetime(2019, 9, 10), datetime(2020, 9, 10)),
+        format="MM/DD/YY")
 
-
-@st.cache
-def get_comments(user_id):
-    query = f"SELECT c.post_id, c.user_id \
-    FROM `bigquery-public-data.stackoverflow.comments` AS c \
-    WHERE c.user_id IN {serialization(user_id)}"
-    query_job = client.query(query, job_config=safe_config)
-    return query_job.to_dataframe()
-
-
-@st.cache
-def get_user_interaction():
-    # query = f"SELECT q.owner_user_id, a.owner_user_id, COUNT(1)\
-    # FROM `bigquery-public-data.stackoverflow.posts_questions` AS q \
-    # INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a \
-    #     ON q.id = a.parent_id \
-    # WHERE a.owner_user_id IN {serialization(user_id)} AND q.owner_user_id IN {serialization(user_id)} \
-    # GROUP BY "
-    users = pd.read_csv('users.csv')
-    user2idx = {u: i for i, u in enumerate(users['id'])}
-    answers = pd.read_csv('answers.csv')
-    uu = np.zeros([len(users), len(users)])
-    for qid, uid in zip(answers['qid'], answers['uid']):
-        uid2 = qid2uid[qid]
-        if uid2 in user2idx:
-            uid, uid2 = user2idx[uid], user2idx[uid2]
-            uu[uid, uid2] += 1
-            uu[uid2, uid] += 1
-    return uu.max(axis=1)
-
-
-@st.cache
-def get_uu_by_comment():
-    users = pd.read_csv('users.csv')
-    u0 = users['id'].to_list()
-    comments = get_comments(u0)
-    st.write(len(comments))
-    user2idx = {u: i for i, u in enumerate(u0)}
-    uu = np.zeros([len(u0), len(u0)])
-    for uid, qid in zip(comments['user_id'], comments['post_id']):
-        if qid < len(qid2uid) and qid2uid[qid] in user2idx:
-            u1 = user2idx[uid]
-            u2 = user2idx[qid2uid[qid]]
-            uu[u1, u2] += 1
-            uu[u2, u1] += 1
-    return uu.max(axis=1)
-
-
-# st.write(get_table('users', 3))
-# st.write(get_answered_questions_by_user(3043))
-# st.write(get_all_users(user_id=[3043, 2493]))
-
-# st.write(get_uu_by_comment())
-
-# df = get_all_users(reputation_thres=1000000)
-# bigV = df['id'].to_list()
-# comments = get_comments(bigV)
-# st.write(len(comments), comments.head())
-
-# word cloud
-df = get_all_users(reputation_thres=100000)
-bigV = df['id'].to_list()
-st.write(len(bigV))
-freq = get_tag_freq_by_user(bigV)
-wc = WordCloud(background_color="white", width=MAX_WIDTH * 2, height=400)
-st.image(wc.generate_from_frequencies(freq).to_image(),
-         use_column_width=True)
-
-# u-u interaction in QA
-p = get_user_interaction()
-p.sort()
-p = p[p > 0]
-fig, ax = plt.subplots()
-ax.plot(range(len(p)), p[::-1])
-st.pyplot(fig)
-# u-u interaction in QA
-p = get_uu_by_comment()
-p.sort()
-p = p[p > 0]
-fig, ax = plt.subplots()
-ax.plot(range(len(p)), p[::-1])
-st.pyplot(fig)
+    user_t = get_user_timeline([2686, 3122, 2795, 4855], *a)
+    get_single_user_timeline(user_t[3122])
+    # user_t.pop(3122)
+    get_multi_user_timeline(user_t)
